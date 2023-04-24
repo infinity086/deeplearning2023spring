cfac4e33389e4007ade82d2111f6308bfb943aef
cccccwwl <90431525+cccccwwl@users.noreply.github.com>
2023-04-24 23:52:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 # first we download the datasets
 # unzip the datasets
-mkdir -p ./data && wget https://data.caltech.edu/records/nyy15-4j048/files/256_ObjectCategories.tar?download=1 && tar -xvf  256_ObjectCategories.tar?download=1 && rm -r 256_ObjectCategories.tar?download=1
+
+cur_dir=$(pwd)
+
+mkdir -p ${cur_dir}/data && wget https://data.caltech.edu/records/nyy15-4j048/files/256_ObjectCategories.tar?download=1 && tar -xvf  256_ObjectCategories.tar?download=1 && rm -r 256_ObjectCategories.tar?download=1
 
 # delete some file for it 
-cd ./256_ObjectCategories && rm -r ./056.dog/greg && rm -r ./198.spider/RENAME2
+cd ${cur_dir}/256_ObjectCategories && rm -r ./056.dog/greg && rm -r ./198.spider/RENAME2
 
-python pictureclassifier.py
+cd ${cur_dir} && python pictureclassifier.py
